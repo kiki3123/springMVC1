0978626feb4f082056a0fafa96b924eef816e585
kiki3123 <sekihwan@naver.com>
2025-02-10 22:22:46 +0900

    서블릿으로 회원 관리 웹 애플리케이션 만들기

--- src/main/java/hello/servlet/domain/member/Member.java
+++ src/main/java/hello/servlet/domain/member/Member.java
@@ -10,9 +10,7 @@ public class Member {
     private String username;
     private int age;
 
-    public Member() {
-
-    }
+    public Member() { }
 
     public Member(String username, int age) {
         this.username = username;
